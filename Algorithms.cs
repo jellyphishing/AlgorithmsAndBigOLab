6c8ac4ae2f3dfc725e320a468bb09428a68a33a2
Lorraine Martin <lolocodes2001@gmail.com>
2023-09-06 15:52:49 -0700

    Loops with conditionals Task 2 & 3

--- Algorithms.cs
+++ Algorithms.cs
@@ -1,31 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Algorithms___Big_O_Lab
 {
     internal class Algorithms
-    //Task 1: Even or Odd
+    //Task 1: Even or Odd (Time Complexity: Constant O(1))
     {
-        static void main() 
+        public bool OddEven(int number)
         {
-              bool number;
-            Console.WriteLine("Enter any number.");
-            bool number = bool.Parse(Console.ReadLine());
 
-
-            if (number%2 == 0)
+            if (number % 2 == 0)
             {
-                Return true;
+                return true;
             }
 
 
             else
             {
-                Return false;
+                return false;
             }
         }
+
+
+        //Task 2: Less Than 100 (Time Complexity: Linear O(n) )
+        public bool LessThanOneHundred(List<int> numberList)
+        {
+            bool isLessThanOneHundred = true;
+
+            for (int i = 0; i < numberList.Count; i++)
+
+            {
+                if (numberList[i] >= 100)
+                {
+                    isLessThanOneHundred = false; ;
+                }
+                else
+                {
+
+                }
+
+            }
+            return isLessThanOneHundred;
+        }
+        //Task 3 Repeated Names( Time Complexity:     )
+        public bool RepeatedNames(List<string> namesList)
+        {
+            bool hasARepeatedName = true;
+            for (int i = 0; i < namesList.Count; i++)
+            {
+                if (namesList[i] != namesList[i])
+                {
+                    hasARepeatedName = false;
+                }
+                else
+                {
+                }
+            }
+
+            return hasARepeatedName;
+        }
+
+        //Task 4 Sort List (Time Complexity:     )
+
+
     }
 }
