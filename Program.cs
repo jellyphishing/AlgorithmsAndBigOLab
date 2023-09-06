5b4fe552d78a6ade03d4bcedeaacd7e063738dc6
Lorraine Martin <lolocodes2001@gmail.com>
2023-09-06 09:14:13 -0700

    :Initial Push

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithms___Big_O_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
